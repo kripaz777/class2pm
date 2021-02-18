32173fc66f695e637d920034bf40d9e4cd8d19df
kripaz777 <aceraayush@gmail.com>
2021-02-18 15:56:52 +0545

    templates

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from .views import *
 app_name = "home"
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def price(request):
+    return render(request,'price.html')
+
+def services(request):
+    return render(request, 'services.html')
